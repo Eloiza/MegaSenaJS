d2444ce243391c277792cb694886eef82eabda6e
Eloiza <eloiza.rossetto@gmail.com>
2021-02-16 16:21:27 -0300

    Adicionando funcionalidade de escolher numeros para o jogo e printa-los no final da pagina

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function createMegaTable(lin, col){
 	for(i=0; i<6; i++){
 		row += '<tr>'
 		for(j=0; j<10; j++){
-			var checkbox = '<input type="checkbox" id=' + item +'value='+ item + '>';
+			var checkbox = '<input type="checkbox" id=' + item +' value='+ item + '>';
 			checkbox += '<label for=' + item + '>' + item + '</label>';
 
 			row += '<th><div>' + checkbox + '</div></th>'
@@ -18,5 +18,31 @@ function createMegaTable(lin, col){
 
 $(document).ready(function(){
 	createMegaTable(6,10);
+
+	var selected_numbers = []
+	let div_numbers = $("#game_numbers");
+	$("input:checkbox").click(function(){
+		index = selected_numbers.indexOf(this.id);
+
+		//found the element...so we remove it
+		if(index > -1){
+			selected_numbers.splice(index,1)
+		}
+
+		//we add the element to the array
+		//and print in the box "game"
+		else{
+			//total number in a game
+			if(selected_numbers.length == 6){
+				console.log("Jogo feito :D - Remova algum item")
+			}
+			else{
+				selected_numbers.push(this.id);
+			}
+		}
+		
+		div_numbers.text(selected_numbers);
+
+	});
 
 });
