1e7c91fe295f152e242a9431cb4c811c5414bfd6
Eloiza <eloiza.rossetto@gmail.com>
2021-02-16 15:56:35 -0300

    Função para criação dinamica da tabela do jogo feita

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//some cool thing here
+function createMegaTable(lin, col){
+	let table = $("#game_table");
+	var row;
+	var item = 1;
+	for(i=0; i<6; i++){
+		row += '<tr>'
+		for(j=0; j<10; j++){
+			var checkbox = '<input type="checkbox" id=' + item +'value='+ item + '>';
+			checkbox += '<label for=' + item + '>' + item + '</label>';
+
+			row += '<th><div>' + checkbox + '</div></th>'
+			item++;
+		}
+		row += '</tr>'		
+	}
+	table.html(row);
+}
+
+$(document).ready(function(){
+	createMegaTable(6,10);
+
+});
