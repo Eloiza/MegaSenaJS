bca8c0e79617ed7bd26a56c1a9b30ba32a3ab881
Eloiza <eloiza.rossetto@gmail.com>
2021-02-17 17:21:09 -0300

    Tentando deixar as coisas bonitas

--- script.js
+++ script.js
@@ -2,17 +2,19 @@ function createMegaTable(lin, col){
 	let table = $("#game_table");
 	var row;
 	var item = 1;
+	row += '<tbody>';
 	for(i=0; i<6; i++){
 		row += '<tr>';
 		for(j=0; j<10; j++){
-			var checkbox = '<input type="checkbox" id=' + item +' value='+ item + '>';
-			checkbox += '<label for=' + item + '>' + item + '</label>';
+			var checkbox = '<input class="custom-control-input" type="checkbox" id=' + item +' value='+ item + '>';
+			checkbox += '<label class="custom-control-label" for=' + item + '>' + item + '</label>';
 
-			row += '<th><div>' + checkbox + '</div></th>';
+			row += '<td><div class="custom-control" custom-checkbox>' + checkbox + '</div></td>';
 			item++;
 		}
 		row += '</tr>';		
 	}
+	row += '</tbody>';
 	table.html(row);
 }
 
